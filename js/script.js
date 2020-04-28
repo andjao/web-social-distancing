497333bc2c0c0250cd1ee3e6ea92ce8ec2180f06
andjao <ads.marchi@gmail.com>
2020-04-28 15:02:35 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,28 +1,29 @@
+let cont;
 let detachment;
-let width;
-let length;
+let x;
+let y;
 let meters2;
-let peoplesW;
-let peoplesL;
+let peoplesX;
+let peoplesY;
 let peoples
-let cont = 0;
 
 setTimeout(function () {
-    calcular();
+    // calcular();
 }, 0);
 
 function calcular() {
+    let cont = 0;
     detachment = document.getElementById('detachment').value;
-    width = document.getElementById('width').value;
-    length = document.getElementById('length').value;
-    meters2 = width * length;
-    peoplesW = Math.ceil(width / detachment);
-    peoplesL = Math.ceil(length / detachment);
-    peoples = peoplesW * peoplesL;
+    x = document.getElementById('width').value;
+    y = document.getElementById('length').value;
+    meters2 = x * y;
+    peoplesX = Math.ceil(x / detachment);
+    peoplesY = Math.ceil(y / detachment);
+    peoples = peoplesX * peoplesY;
     let drawing = `<div class='grid'>`;
-    for (let x = 1; x <= peoplesW; x++) {
+    for (let y = 1; y <= peoplesY; y++) {
         drawing += `<div class='row'>`;
-        for (let y = 1; y <= peoplesL; y++) {
+        for (let x = 1; x <= peoplesX; x++) {
             cont++;
             drawing += `<div class="box">
 <div class='inner' id='p${cont}' onmouseover='neighbors(${cont}, ${x}, ${y}, true)' onmouseout='neighbors(${cont}, ${x}, ${y}, false)'>${cont}</div>
@@ -35,28 +36,28 @@ function calcular() {
     print += `<br><p>Distanciamento Social: ${detachment}</p>`;
     print += `<p>Metros quadrados do estabelecimento: ${meters2}</p>`;
     print += `<p>Quantidade de pessoas permitidas no estabelecimento: ${peoples}</p>`;
-    document.body.innerHTML += print;
+    document.getElementById('result').innerHTML = print;
 }
 
 function neighbors(people, posX, posY, show) {
-    const top = posX !== 1;
-    const left = posY !== 1;
-    const right = posY !== peoplesW;
-    const bottom = posX !== peoplesL;
+    const top = posY !== 1;
+    const left = posX !== 1;
+    const right = posX !== peoplesX;
+    const bottom = posY !== peoplesY;
     const topLeft = top && left;
     const topRight = top && right;
     const bottomLeft = bottom && left;
     const bottomRight = bottom && right;
 
-    if (topLeft) show ? addClass(`p${people - peoplesL - 1}`, 'collateralC') : removeClass(`p${people - peoplesL - 1}`, 'collateralC');
-    if (top) show ? addClass(`p${people - peoplesL}`, 'cardinalC') : removeClass(`p${people - peoplesL}`, 'cardinalC');
-    if (topRight) show ? addClass(`p${people - peoplesL + 1}`, 'collateralC') : removeClass(`p${people - peoplesL + 1}`, 'collateralC');
+    if (topLeft) show ? addClass(`p${people - peoplesX - 1}`, 'collateralC') : removeClass(`p${people - peoplesX - 1}`, 'collateralC');
+    if (top) show ? addClass(`p${people - peoplesX}`, 'cardinalC') : removeClass(`p${people - peoplesX}`, 'cardinalC');
+    if (topRight) show ? addClass(`p${people - peoplesX + 1}`, 'collateralC') : removeClass(`p${people - peoplesX + 1}`, 'collateralC');
     if (left) show ? addClass(`p${people - 1}`, 'cardinalC') : removeClass(`p${people - 1}`, 'cardinalC');
     show ? addClass(`p${people}`, 'peopleC') : removeClass(`p${people}`, 'peopleC');
     if (right) show ? addClass(`p${people + 1}`, 'cardinalC') : removeClass(`p${people + 1}`, 'cardinalC');
-    if (bottomLeft) show ? addClass(`p${people + peoplesL - 1}`, 'collateralC') : removeClass(`p${people + peoplesL - 1}`, 'collateralC');
-    if (bottom) show ? addClass(`p${people + peoplesL}`, 'cardinalC') : removeClass(`p${people + peoplesL}`, 'cardinalC');
-    if (bottomRight) show ? addClass(`p${people + peoplesL + 1}`, 'collateralC') : removeClass(`p${people + peoplesL + 1}`, 'collateralC');
+    if (bottomLeft) show ? addClass(`p${people + peoplesX - 1}`, 'collateralC') : removeClass(`p${people + peoplesX - 1}`, 'collateralC');
+    if (bottom) show ? addClass(`p${people + peoplesX}`, 'cardinalC') : removeClass(`p${people + peoplesX}`, 'cardinalC');
+    if (bottomRight) show ? addClass(`p${people + peoplesX + 1}`, 'collateralC') : removeClass(`p${people + peoplesX + 1}`, 'collateralC');
 }
 
 function addClass(people, classC) {
